52c69ffafcec269525c1969adc92f35c9fd2ad0f
ongkitwei <ongkitwei@gmail.com>
2022-11-22 11:12:35 +0800

    finally can run python script in ssd and copy it

--- copy.py
+++ copy.py
@@ -6,54 +6,46 @@ import shutil
 
 keyword1 = ".bag"
 keyword2 = ".mp4"
-list_file = " "
+#list_file = " "
 total_files = 0
 files_copied = 0
 
 create_folder = input("Name of folder to create: \n")
 
-directory = os.getcwd()
-list_file = os.listdir(directory)
+directory_ssd = os.getcwd()
+#print(directory_ssd)
+list_directory_ssd = os.listdir(directory_ssd)
 
-video_directory = r'/home/kitwei/Videos '   #change accordingly
+bag_directory = r'/home/kitwei/ftp/recorded_bags'
+list_bag_directory = os.listdir(bag_directory)
+#print(list_bag_directory)
 
-
-#adding the r converts the normal string to a raw string
-destination_dir = r'/media/kitwei/Extreme SSD'     #change this according to ur thumbdrive directory
-path = os.path.join(destination_dir + "/", create_folder)
+path = os.path.join(directory_ssd + "/", create_folder)
 os.makedirs(path)
 
-for filename in list_file:
-    if keyword1 in filename:
+
+
+
+for filename in list_bag_directory:
+    if filename.endswith('.bag'):   
         total_files = total_files + 1
 
 
 
+for filename in list_bag_directory:
+    if filename.endswith('.bag'):
+        full_directory = path + "/" + filename
+        #print(full_directory)
 
-for filename in list_file:
-    if keyword1 in filename:
+        bag_directory_full = bag_directory + "/" + filename
+        #print(bag_directory_full)
+
+        shutil.copy(bag_directory_full, full_directory)
         files_copied = files_copied + 1
-        full_directory = directory + "/" + filename
-        destination_dir_cpfile = path + "/" + filename
-        #print(destination_dir_cpfile)
-        shutil.copy(full_directory, destination_dir_cpfile)
         print(str(files_copied) + "/" + str(total_files))
-        #print(os.listdir(destination_dir))
-            
-        #os.listdir returns everyth in a file
-
-        #print(count)
-        
-
 
 
 
 
 print(os.listdir(path))
 print("copy paste done!")
-
-
-
-#/home/kitwei/Videos directroy for screen recorded video
-
-
